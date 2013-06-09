86566a44baf77d7d120fdbb4e406bfbb580714c6
Ricardo Silva Veloso <ricvelozo@gmail.com>
2013-06-09 03:43:51 -0300

    Replace static method and class with a function, and add a little description

--- extract-blend.py
+++ extract-blend.py
@@ -1,65 +1,63 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-
 
-""" Author:  Ricardo Silva Veloso (Ricvelozo)
-    Website: http://ricvelozo.blogspot.com/
-
+"""Extract a .blend file of a Windows executable (.exe)
+Author:  Ricardo Silva Veloso (Ricvelozo)
+Website: http://ricvelozo.blogspot.com/
 """
 
 import os
 import re
 import sys
 
-class Blend_file:
-    @staticmethod
-    def extract(executable_url):
-        blend_dir, exe_name = os.path.split(executable_url)
-        blend_name = exe_name[0:-3] + "blend"
+def extract_blend(executable_url):
+    blend_dir, exe_name = os.path.split(executable_url)
+    blend_name = exe_name[0:-3] + "blend"
 
-        try:
-            executable_file = open(executable_url, "rb")
-            print("Processing \"" + exe_name + "\"...")
+    try:
+        executable_file = open(executable_url, "rb")
+        print("Processing \"" + exe_name + "\"...")
 
-            line = b""
-            while True:
-                line_temp = executable_file.read(1024)
-                if line_temp == b"":
-                    print("The file \"" + exe_name + "\" does not have a " \
-                          ".blend file")
-                    break
+        line = b""
+        while True:
+            line_temp = executable_file.read(1024)
+            if line_temp == b"":
+                print("The file \"" + exe_name + "\" does not have a " \
+                      ".blend file")
+                break
 
-                line = line + line_temp
-                pos = re.search(b"BLENDER_[v|V][0-9]{3}REND", line)
-                if not pos == None:
-                    try:
-                        blend_file = open(os.path.join(blend_dir, blend_name),
-                                          "wb")
-                        pos = pos.start()
+            line = line + line_temp
+            pos = re.search(b"BLENDER_[v|V][0-9]{3}REND", line)
+            if not pos == None:
+                try:
+                    blend_file = open(os.path.join(blend_dir, blend_name),
+                                      "wb")
+                    pos = pos.start()
 
-                        print("Extracting \"" + exe_name + "\"...")
-                        blend_file.write(line[pos:len(line)])
+                    print("Extracting \"" + exe_name + "\"...")
+                    blend_file.write(line[pos:len(line)])
 
-                        while True:
-                            line_temp = executable_file.read(1024)
-                            if line_temp == b"":
-                                break
+                    while True:
+                        line_temp = executable_file.read(1024)
+                        if line_temp == b"":
+                            break
 
-                            blend_file.write(line_temp)
+                        blend_file.write(line_temp)
 
-                        blend_file.close()
-                        print(".blend file extracted: \"" + blend_name + "\"")
+                    blend_file.close()
+                    print(".blend file extracted: \"" + blend_name + "\"")
 
-                    except IOError:
-                        print("Could not create the .blend file \"" +
-                              blend_name + "\"!")
+                except IOError:
+                    print("Could not create the .blend file \"" + blend_name +
+                          "\"!")
 
-                    break
-                
-                line = line_temp[-32:len(line_temp)]
+                break
+            
+            line = line_temp[-32:len(line_temp)]
 
-        except IOError:
-            print("Could not open the file \"" + exe_name + "\"")
-            return False
+    except IOError:
+        print("Could not open the file \"" + exe_name + "\"")
+        return False
 
 
 if len(sys.argv) > 1:
@@ -67,7 +65,7 @@ if len(sys.argv) > 1:
 
     # Extract the .blend file(s)
     for arg in args:
-        Blend_file.extract(arg)
+        extract_blend(arg)
 
 else:
     while True:
@@ -84,4 +82,4 @@ else:
                 executable_url = executable_url[0:-1]
 
             # Extract the .blend file
-            Blend_file.extract(executable_url)
+            extract_blend(executable_url)
