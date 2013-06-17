f7406df7d5007f7021593d6fe8dcd05c9d873d89
Ricardo Silva Veloso <ricvelozo@gmail.com>
2013-06-17 03:57:11 -0300

    Add 'main' function, and use single quotation inside strings

--- extract-blend.py
+++ extract-blend.py
@@ -16,14 +16,14 @@ def extract_blend(executable_url):
 
     try:
         executable_file = open(executable_url, "rb")
-        print("Processing \"" + exe_name + "\"...")
+        print("Processing '" + exe_name + "'...")
 
         line = b""
         while True:
             line_temp = executable_file.read(1024)
             if line_temp == b"":
-                print("The file \"" + exe_name + "\" does not have a " \
-                      ".blend file")
+                print("The file '" + exe_name + "' does not have a .blend " \
+                      "file")
                 break
 
             line = line + line_temp
@@ -34,7 +34,7 @@ def extract_blend(executable_url):
                                       "wb")
                     pos = pos.start()
 
-                    print("Extracting \"" + exe_name + "\"...")
+                    print("Extracting '" + exe_name + "'...")
                     blend_file.write(line[pos:len(line)])
 
                     while True:
@@ -45,41 +45,44 @@ def extract_blend(executable_url):
                         blend_file.write(line_temp)
 
                     blend_file.close()
-                    print(".blend file extracted: \"" + blend_name + "\"")
+                    print(".blend file extracted: '" + blend_name + "'")
 
                 except IOError:
-                    print("Could not create the .blend file \"" + blend_name +
-                          "\"!")
+                    print("Could not create the .blend file '" + blend_name +
+                          "'")
 
                 break
             
             line = line_temp[-32:len(line_temp)]
 
     except IOError:
-        print("Could not open the file \"" + exe_name + "\"")
+        print("Could not open the file '" + exe_name + "'")
         return False
 
+def main():
+    if len(sys.argv) > 1:
+        args = sys.argv[1:len(sys.argv)]
 
-if len(sys.argv) > 1:
-    args = sys.argv[1:len(sys.argv)]
+        # Extract the .blend file(s)
+        for arg in args:
+            extract_blend(arg)
 
-    # Extract the .blend file(s)
-    for arg in args:
-        extract_blend(arg)
+    else:
+        while True:
+            executable_url = input("Enter the URL of the executable file, " \
+                                   "or ENTER to exit:\n>>> ")
 
-else:
-    while True:
-        executable_url = input("Enter the URL of the executable file, or " \
-                               "ENTER to exit:\n>>> ")
+            # Empty line on Unix-like and Windows, respectively
+            if len(executable_url) == 0 or executable_url == '\r':
+                break
 
-        # Empty line on Unix-like and Windows, respectively
-        if len(executable_url) == 0 or executable_url == '\r':
-            break
+            else:
+                # Remove the '\r' character on Windows
+                if executable_url[-1] == '\r':
+                    executable_url = executable_url[0:-1]
 
-        else:
-            # Remove the '\r' character on Windows
-            if executable_url[-1] == '\r':
-                executable_url = executable_url[0:-1]
+                # Extract the .blend file
+                extract_blend(executable_url)
 
-            # Extract the .blend file
-            extract_blend(executable_url)
+if __name__ == "__main__":
+    main()
