0f2eefc62ca9d5a3cf39a3be2d6ab5647bc127cc
Ricardo Silva Veloso <ricvelozo@gmail.com>
2013-12-23 14:21:05 -0200

    Fix LAMP script

--- LAMP.sh
+++ LAMP.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
-sudo apt-get update && sudo apt-get install mysql-server apache2 libapache2-mod-php5 php5 php5-mysql phpmyadmin
+sudo apt-get install mysql-server apache2 libapache2-mod-php5 php5 php5-mysql phpmyadmin
 
 if [ $(grep --count "/etc/phpmyadmin/apache.conf" \
-      "/etc/apache2/apache2.conf") = "0" ]
+     "/etc/apache2/apache2.conf") = "0" ]
 then
     # Include the configuration of phpMyAdmin for Apache
-    sudo echo -e "\n# phpMyAdmin\nInclude /etc/phpmyadmin/apache.conf" \
-    >> /etc/apache2/apache2.conf
+    echo -e "\n# phpMyAdmin\nInclude /etc/phpmyadmin/apache.conf" \
+    | sudo tee -a /etc/apache2/apache2.conf
+
+    # Create `/etc/apache2/conf.d/` directory if it does not exist
+    sudo mkdir -p /etc/apache2/conf.d/
 
     # Create a symbolic link to configuration of phpMyAdmin
     sudo ln -s /etc/phpmyadmin/apache.conf /etc/apache2/conf.d/phpmyadmin.conf
 
     # Restart the Apache
     sudo service apache2 restart
-fi
-
-# Create a symbolic link to /var/www/ in home directory
-if [ $(ls ~/ | grep --count "^www$") = "0" ]
-    then sudo ln -s /var/www/ ~/www
 fi
