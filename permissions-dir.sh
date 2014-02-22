5e6cfe1d0ad609bada014f6dfdb8048bb14d5887
Ricardo Silva Veloso <ricvelozo@gmail.com>
2014-02-22 20:16:16 -0300

    Fix typo in comment

--- permissions-dir.sh
+++ permissions-dir.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# Set permissions 775 for direcories, and 664 for files, recursively
+# Set permissions 775 for directories, and 664 for files, recursively
 # Source: http://superuser.com/a/51873
 find . \( -type d -exec chmod 775 {} \; \) -o \( -type f -exec chmod 664 {} \; \)
