29534b0c4f261688d26a4961835ab893c36c0dbd
Ricardo Silva Veloso <ricvelozo@gmail.com>
2014-09-21 11:53:39 -0300

    Add prepare-dir script for web

---
+++ prepare-dir-web.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Get the user directories
+cat ~/.config/user-dirs.dirs | while read line; do
+    if [ "${line:0:17}" = "XDG_TEMPLATES_DIR" ]; then
+        # Get the current Templates directory
+        eval templates_dir="${line:19:-1}"
+
+        # Copy README* template(s), if any
+        if [ $(ls "$templates_dir/README"* 2> /dev/null | wc -l) != "0" ]
+            then cp -n -r -T "$templates_dir/README"* ./
+        fi
+    fi
+done
+
+# Make the basic directory structure
+mkdir -p assets content && cd assets && mkdir -p css fonts images js scss
