f66c2549a031f52b51599cab934f2d0a8ac35811
Ricardo Silva Veloso <ricvelozo@gmail.com>
2017-04-12 02:22:44 -0300

    Use xdg-user-dir tool instead of read the config file directly

--- prepare-dir-web.sh
+++ prepare-dir-web.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-# Get the user directories
-cat ~/.config/user-dirs.dirs | while read line; do
-    if [ "${line:0:17}" = "XDG_TEMPLATES_DIR" ]; then
-        # Get the current Templates directory
-        eval templates_dir="${line:19:-1}"
+# Get the current Templates directory
+templates_dir="$(xdg-user-dir TEMPLATES)"
 
-        # Copy README* template(s), if any
-        if [ $(ls "$templates_dir/README"* 2> /dev/null | wc -l) != "0" ]
-            then cp -n -r -T "$templates_dir/README"* ./
-        fi
-    fi
-done
+# Copy README* template(s), if any
+if [ $(ls "$templates_dir/README"* 2> /dev/null | wc -l) != "0" ]
+    then cp -n -r -T "$templates_dir/README"* ./
+fi
 
 # Make the basic directory structure
 mkdir -p public/content resources/assets && cd resources/assets &&
