08fe2d926cdc33a270131fb3be9744eb2cf73896
Ricardo Silva Veloso <ricvelozo@gmail.com>
2017-09-09 17:28:06 -0300

    Update comments in (.deb) install scripts

--- install-insomnia-rest-client-deb.sh
+++ install-insomnia-rest-client-deb.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+# Installs or updates the Insomnia REST Client (.deb)
 cd /tmp && wget https://builds.insomnia.rest/downloads/ubuntu/latest -O \
 insomnia-rest-client.deb && sudo dpkg -i ./insomnia-rest-client.deb
