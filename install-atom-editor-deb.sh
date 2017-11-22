5fd1924d6108710a2918a60932db9a82387e47ba
Ricardo Silva Veloso <ricvelozo@gmail.com>
2017-11-21 22:13:29 -0200

    Add script to install Atom Editor (.rpm) and favorite packages

--- install-atom-editor-deb.sh
+++ install-atom-editor-deb.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Installs or updates the Atom Editor (.deb) with favorite packages
 cd /tmp && wget https://atom.io/download/deb -O atom-editor.deb &&
-sudo dpkg -i ./atom-editor.deb && apm install sublime-style-column-selection \
-atom-ide-ui ide-css ide-php ide-rust ide-typescript ide-vue language-vue emmet \
-color-picker file-icons minimap
+sudo dpkg -i ./atom-editor.deb && apm install atom-ide-ui ide-css ide-php \
+ide-rust ide-typescript ide-vue language-vue emmet color-picker file-icons \
+minimap sublime-style-column-selection
---
+++ install-atom-editor-rpm.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# Installs or updates the Atom Editor (.rpm) with favorite packages
+cd /tmp && wget https://atom.io/download/rpm -O atom-editor.rpm &&
+sudo dnf install ./atom-editor.rpm && apm install atom-ide-ui ide-css ide-php \
+ide-rust ide-typescript ide-vue language-vue emmet color-picker file-icons \
+minimap sublime-style-column-selection
