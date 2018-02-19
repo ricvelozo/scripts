4178b4cce0b30ae841a2cf9254f35621cc9a29bc
Ricardo Silva Veloso <ricvelozo@gmail.com>
2018-02-19 00:43:46 -0300

    Request password earlier and make wget continue an unsuccessful download

--- install-atom-editor-deb.sh
+++ install-atom-editor-deb.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Installs or updates the Atom Editor (.deb) with favorite packages
-cd /tmp && wget https://atom.io/download/deb -O atom-editor.deb &&
+cd /tmp && sudo wget -c https://atom.io/download/deb -O atom-editor.deb &&
 sudo dpkg -i ./atom-editor.deb && apm install atom-ide-ui ide-css ide-php \
 ide-rust ide-typescript ide-vue language-vue emmet color-picker file-icons \
 minimap sublime-style-column-selection
