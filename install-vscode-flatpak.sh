fe66d848e802a63d69ee36c3e3f1b66eee11279b
Ricardo Silva Veloso <ricvelozo@gmail.com>
2019-03-25 01:04:32 -0300

    Update theme in VS Code installation script

--- install-vscode-flatpak.sh
+++ install-vscode-flatpak.sh
@@ -5,7 +5,7 @@ https://dl.flathub.org/repo/flathub.flatpakrepo && flatpak install flathub \
 com.visualstudio.code.oss && flatpak run com.visualstudio.code.oss \
 --install-extension ms-ceintl.vscode-language-pack-pt-br \
 --install-extension pkief.material-icon-theme \
---install-extension equinusocio.vsc-material-theme \
+--install-extension zhuangtongfa.material-theme \
 --install-extension EditorConfig.EditorConfig \
 --install-extension formulahendry.auto-close-tag \
 --install-extension formulahendry.auto-rename-tag \
