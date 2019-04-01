e1422a230e2f5ffb3de314fff6cc6e60722928e8
Ricardo Silva Veloso <ricvelozo@gmail.com>
2019-04-01 07:56:12 -0300

    Update extensions in VS Code installation script

--- install-vscode-flatpak.sh
+++ install-vscode-flatpak.sh
@@ -3,13 +3,15 @@
 flatpak remote-add --if-not-exists flathub \
 https://dl.flathub.org/repo/flathub.flatpakrepo && flatpak install flathub \
 com.visualstudio.code.oss && flatpak run com.visualstudio.code.oss \
---install-extension ms-ceintl.vscode-language-pack-pt-br \
---install-extension pkief.material-icon-theme \
 --install-extension zhuangtongfa.material-theme \
+--install-extension pkief.material-icon-theme \
 --install-extension EditorConfig.EditorConfig \
+--install-extension yzhang.markdown-all-in-one \
+--install-extension dotjoshjohnson.xml \
 --install-extension formulahendry.auto-close-tag \
 --install-extension formulahendry.auto-rename-tag \
 --install-extension dbaeumer.vscode-eslint \
 --install-extension octref.vetur \
 --install-extension ms-python.python \
---install-extension rust-lang.rust
+--install-extension rust-lang.rust \
+--install-extension bungcip.better-toml
