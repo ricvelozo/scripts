404055ca33fcb1e38b26f60467dcbdb9310c799e
Ricardo Silva Veloso <ricvelozo@gmail.com>
2019-12-11 15:30:58 -0300

    Update VS Code installation script

--- install-vscode-flatpak.sh
+++ install-vscode-flatpak.sh
@@ -16,25 +16,24 @@ flatpak run com.visualstudio.code \
 --install-extension bmewburn.vscode-intelephense-client \
 --install-extension ms-python.python \
 --install-extension rust-lang.rust \
---install-extension bungcip.better-toml
+--install-extension bungcip.better-toml \
+--install-extension geequlim.godot-tools
 
 # settings.json:
 # {
-#    "window.menuBarVisibility": "toggle",
-#    "workbench.colorTheme": "One Dark Pro",
-#    "workbench.iconTheme": "material-icon-theme",
-#    "editor.rulers": [
-#        80,
-#        100
-#    ],
-#    "files.insertFinalNewline": true,
-#    "files.watcherExclude": {
-#        "**/target/**": true
-#    },
-#    "terminal.integrated.shell.linux": "flatpak-spawn",
-#    "terminal.integrated.shellArgs.linux": ["--host", "bash"],
-#    "eslint.nodePath": "/home/ricardo/flatpak-nodejs",
-#    "php.validate.executablePath": "/home/ricardo/flatpak-php",
-#    "rust-client.rlsPath": "/home/ricardo/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/bin/rls",
-#    "telemetry.enableTelemetry": false
+#     "window.menuBarVisibility": "toggle",
+#     "workbench.colorTheme": "One Dark Pro",
+#     "workbench.iconTheme": "material-icon-theme",
+#     "editor.rulers": [
+#         80,
+#         100
+#     ],
+#     "files.insertFinalNewline": true,
+#     "files.watcherExclude": {
+#         "**/target/**": true
+#     },
+#     "eslint.nodePath": "/home/ricardo/flatpak-nodejs",
+#     "php.validate.executablePath": "/home/ricardo/flatpak-php",
+#     "rust-client.rustupPath": "/home/ricardo/.cargo/bin/rustup",
+#     "telemetry.enableTelemetry": false
 # }
