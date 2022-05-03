277f829f80ef0bd0bfe3d4c81261c3f78225f0ea
Ricardo Silva Veloso <ricvelozo@gmail.com>
2022-05-03 19:52:55 -0300

    Refactor extract_blend.py

--- extract-blend.py
+++ extract-blend.py
@@ -4,50 +4,50 @@
 Website: https://gitlab.com/ricvelozo/scripts
 """
 
-__version__ = '2.2.1'
+__version__ = '2.3.0'
 __author__ = 'Ricardo Silva Veloso'
 
 import click
 import re
-from io import DEFAULT_BUFFER_SIZE
-from os import path
+from pathlib import Path
 
 @click.command(context_settings=dict(help_option_names=['-h', '--help']))
 @click.version_option(version=__version__, message='%(prog)s %(version)s')
 @click.argument('executables', metavar='INPUT', nargs=-1)
 def extract(executables: list[str]):
     """Extract a .blend file from a Windows executable (.exe)."""
-    prog = re.compile(b'BLENDER[_|-][v|V][0-9]{3}')
+    magic_number_prog = re.compile(b'BLENDER[_|-][v|V][0-9]{3}')
     if not executables:
-        executable = input('Enter the URL of the executable file, ' \
-                           'or ENTER to exit:\n>>> ').strip()
-        if executable:
-            executables = [executable]
-    for i, executable in enumerate(executables):
-        dir, filename = path.split(executable)
-        blend = filename[:-4] if filename.endswith('.exe') else filename
-        blend += '.blend'
-        if i > 0:
-            print()
+        executables = [input('Enter the URL of the executable file, ' \
+                             'or ENTER to exit:\n>>> ').strip()]
+    executables = [Path(exe) for exe in executables if exe]
+    for i, exe_path in enumerate(executables):
+        blend_path = exe_path.with_suffix('.blend')
+        exe_name, blend_name = exe_path.name, blend_path.name
         try:
-            with open(executable, 'rb') as exe:
-                print(f'{[i]} Processing `{filename}`...')
-                bigger_chunk = b''
-                while chunk := exe.read(DEFAULT_BUFFER_SIZE):
+            if i > 0:
+                print()
+            with exe_path.open('rb') as exe:
+                print(f'{[i]} Processing `{exe_name}`...')
+                bigger_chunk = bytes()
+
+                # Search for the .blend file's magic number
+                while chunk := exe.read1():
                     bigger_chunk += chunk
-                    if magic_number := prog.search(bigger_chunk):
-                        print(f'{[i]} Extracting `{blend}`...')
+                    if magic_number := magic_number_prog.search(bigger_chunk):
+                        print(f'{[i]} Extracting `{blend_name}`...')
                         try:
-                            with open(path.join(dir, blend), 'wb') as output:
+                            with blend_path.open('wb') as blend:
                                 # Copy the .blend file's start
                                 start = magic_number.start()
-                                output.write(bigger_chunk[start:])
+                                blend.write(bigger_chunk[start:])
+                                del bigger_chunk
 
                                 # Copy the rest of the .blend file
-                                while chunk := exe.read(DEFAULT_BUFFER_SIZE):
-                                    output.write(chunk)
+                                while chunk := exe.read1():
+                                    blend.write(chunk)
                         except IOError:
-                            print(f'{[i]} Could not write to `{blend}`.')
+                            print(f'{[i]} Could not write to `{blend_name}`.')
                         break
                     else:
                         # Keep only the last 12 bytes for the next search
@@ -55,10 +55,10 @@ def extract(executables: list[str]):
 
                 # EOF
                 if magic_number is None:
-                    print(f'{[i]} The file `{filename}` does not have a ' \
+                    print(f'{[i]} The file `{exe_name}` does not have a ' \
                            '.blend file.')
         except IOError:
-            print(f'{[i]} Could not open the file `{filename}`.')
+            print(f'{[i]} Could not open the file `{exe_name}`.')
 
 if __name__ == '__main__':
     extract()
